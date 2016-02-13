3397e194ab562bda635fad5b4d6eb3722ee05d6e
camillecastillo15@gmail.com <camillecastillo@MacBook-Pro-de-Camille.local>
2016-02-13 12:40:01 +0100

    Création de MakerSeed + VehiclesSeed +modifs DatabaseSeeder (pour rentrer des données au hasard dans la BDD)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,21 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use App\Maker;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UserTableSeeder::class);
-    }
+class DatabaseSeeder extends Seeder {
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+		Maker::truncate();
+		Model::unguard();
+		$this->call('MakerSeed');
+		$this->call('VehiclesSeed');
+	}
 }
