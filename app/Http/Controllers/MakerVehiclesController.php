d5d8be7ad51e08c0553f0b598175e152731d7469
camillecastillo15@gmail.com <camillecastillo@MacBook-Pro-de-Camille.local>
2016-02-21 23:38:47 +0100

    création de la table User et de l'identification

--- app/Http/Controllers/MakerVehiclesController.php
+++ app/Http/Controllers/MakerVehiclesController.php
@@ -11,6 +11,12 @@ use App\Vehicle;
 use App\Http\Requests\CreateVehicleRequest;
 
 class MakerVehiclesController extends Controller {
+	
+	public function __construct()
+	{
+		$this->middleware('auth.basic', ['except' => ['index', 'show']]);
+	}
+
 	/**
 	 * Display a listing of the resource.
 	 *
